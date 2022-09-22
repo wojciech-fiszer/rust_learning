f6a11f94dd1e6b91f6e922fbbbe80f27237461fc
Wojciech Fiszer <fiszer.wojciech@gmail.com>
2022-09-22 21:26:48 +0200

    Polishing db

--- todo_app/src/infrastructure/db.rs
+++ todo_app/src/infrastructure/db.rs
@@ -1,39 +1,72 @@
 use crate::domain::todo::{Todo, TodoRepository};
 use anyhow::{anyhow, Result};
 use async_trait::async_trait;
-use sqlx::{Pool, Postgres};
+use sqlx::postgres::PgRow;
+use sqlx::types::Uuid;
+use sqlx::{query_as, Error, FromRow, Pool, Postgres, Row};
 
 pub struct PostgresTodoRepository<'a> {
     pool: &'a Pool<Postgres>,
 }
 
+impl FromRow<'_, PgRow> for Todo {
+    fn from_row(row: &PgRow) -> std::result::Result<Self, Error> {
+        Ok(Todo {
+            id: Some(row.try_get::<Uuid, &str>("id")?.to_string()),
+            title: row.try_get("title")?,
+            done: row.try_get("done")?,
+        })
+    }
+}
+
 #[async_trait]
 impl<'a> TodoRepository for PostgresTodoRepository<'a> {
     async fn save(&self, todo: Todo) -> Result<Todo> {
+        let title = todo.title;
+        let done = todo.done;
         match todo.id {
-            Some(id) => match sqlx::query_as!(
-            Todo,
-            "UPDATE todos SET (title, done) = ($1, $2) WHERE id::text = $3 RETURNING id::text, title, done",
-            todo.title,
-            todo.done,id
-        ).fetch_one(self.pool).await {
-                Ok(todo) => Ok(todo),
-                Err(err) => Err(anyhow!(err))
-            },
-            None => match sqlx::query_as!(
-            Todo,
-            "INSERT INTO todos (title, done) VALUES ($1, $2) RETURNING id::text, title, done",
-            todo.title,
-            todo.done,
-        ).fetch_one(self.pool).await {
-                Ok(todo) => Ok(todo),
-                Err(err) => Err(anyhow!(err))
-            }
+            Some(id) => self.update(id, title, done).await,
+            None => self.create(title, done).await,
         }
     }
 
     async fn get_all(&self) -> Result<Vec<Todo>> {
-        todo!()
+        match query_as("SELECT id, title, done FROM todos")
+            .fetch_all(self.pool)
+            .await
+        {
+            Ok(todos) => Ok(todos),
+            Err(err) => Err(anyhow!(err)),
+        }
+    }
+}
+
+impl<'a> PostgresTodoRepository<'a> {
+    async fn update(&self, id: String, title: String, done: bool) -> Result<Todo> {
+        match query_as(
+            "UPDATE todos SET (title, done) = ($1, $2) WHERE id::text = $3 RETURNING id, title, done",
+        )
+        .bind(title)
+        .bind(done)
+        .bind(id)
+        .fetch_one(self.pool)
+        .await
+        {
+            Ok(todo) => Ok(todo),
+            Err(err) => Err(anyhow!(err)),
+        }
+    }
+
+    async fn create(&self, title: String, done: bool) -> Result<Todo> {
+        match query_as("INSERT INTO todos (title, done) VALUES ($1, $2) RETURNING id, title, done")
+            .bind(title)
+            .bind(done)
+            .fetch_one(self.pool)
+            .await
+        {
+            Ok(todo) => Ok(todo),
+            Err(err) => Err(anyhow!(err)),
+        }
     }
 }
 
--- todo_app/src/main.rs
+++ todo_app/src/main.rs
@@ -8,10 +8,14 @@ use tokio;
 
 #[tokio::main]
 async fn main() -> io::Result<()> {
-    let listener = TcpListener::bind("127.0.0.1:0").expect("Failed to bind random port");
+    let listener = TcpListener::bind("127.0.0.1:8080").expect("Failed to bind random port");
     let db_url = "postgres://todo_app:todo_app@127.0.0.1:5432/todo_app";
     let db_pool = PgPool::connect(db_url)
         .await
         .expect("Cannot connect to database");
+    println!(
+        "Starting application on port: {}",
+        listener.local_addr().unwrap().port()
+    );
     run_api(listener, db_pool)?.await
 }
